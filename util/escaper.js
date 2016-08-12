eb7076e9ea09a66c94a37806afc23707cd3d877e
XadillaX <admin@xcoder.in>
2016-08-12 15:27:17 +0800

    feat: finish mysql adapter makeFieldWhere

---
+++ util/escaper.js
@@ -0,0 +1,42 @@
+/**
+ * XadillaX created at 2016-08-12 12:08:43 With ♥
+ *
+ * Copyright (c) 2016 Souche.com, all rights
+ * reserved.
+ */
+"use strict";
+
+const Escaper = module.exports = {};
+
+Escaper.escape = function(str) {
+    if(typeof str !== "string") {
+        return str;
+    }
+
+    return str.split("").reduce(function(str, ch) {
+        switch(ch) {
+            case "\n"   : str += "\\n"; break;
+            case "'"    : str += "\\'"; break;
+            case "\""   : str += "\\\""; break;
+            case "\t"   : str += "\\t"; break;
+            case "\0"   : str += "\\0"; break;
+            case "\r"   : str += "\\r"; break;
+            case "\b"   : str += "\\b"; break;
+            case "\x1a" : str += "\\Z"; break;
+            case "\\"   : str += "\\\\"; break;
+
+            default: str += ch; break;
+        }
+        return str;
+    }, "");
+};
+
+Escaper.escapeLike = function(str) {
+    return str.split("").reduce(function(str, ch) {
+        if(ch === "_" || ch === "%") {
+            str += "\\";
+        }
+        str += ch;
+        return str;
+    }, "");
+};
