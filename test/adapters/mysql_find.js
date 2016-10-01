fae20054d19d745280ee59b932e5091c685f73a0
XadillaX <admin@xcoder.in>
2016-10-01 11:57:45 +0800

    test: refactor structure

---
+++ test/adapters/mysql_find.js
@@ -0,0 +1,182 @@
+/**
+ * XadillaX created at 2016-10-01 11:48:17 With ♥
+ *
+ * Copyright (c) 2016 Souche.com, all rights
+ * reserved.
+ */
+"use strict";
+
+const should = require("should");
+
+const common = require("../util/common");
+const Query = require("../../lib/query");
+const Toshihiko = require("../../lib/toshihiko");
+
+module.exports = function(name, options) {
+    describe(`${name} findWithNoCache`, function() {
+        const toshihiko = new Toshihiko("mysql", options);
+        const adapter = toshihiko.adapter;
+        const model = toshihiko.define("test1", common.COMMON_SCHEMA);
+    
+        after(function() {
+            adapter.mysql.end();
+        });
+    
+        it("normal 1", function(done) {
+            const query = new Query(model).fields("key1,key2,key3").order("key1 asc").limit(100);
+    
+            adapter.findWithNoCache(query.model, function(err, rows, extra) {
+                should.ifError(err);
+                extra.should.equal("SELECT `id`, `key2`, `key3` FROM `test1` ORDER BY `id` ASC LIMIT 100");
+                rows.should.match([
+                    { id: 1, key2: 0.5, key3: "{\"foo\":\"bar\"}" },
+                    { id: 2, key2: 0.5, key3: "{\"foo\":\"bar\"}" },
+                    { id: 3, key2: 0.5, key3: "{\"foo\":\"bar\"}" },
+                    { id: 4, key2: 0.5, key3: "{\"foo\":\"bar\"}" }
+                ]);
+                done();
+            }, adapter.queryToOptions(query, {}));
+        });
+    
+        it("normal 2", function(done) {
+            const query = new Query(model).fields("key1,key2,key3").order("key2 asc").limit(100);
+    
+            adapter.findWithNoCache(query.model, function(err, rows, extra) {
+                should.ifError(err);
+                extra.should.equal("SELECT `id`, `key2`, `key3` FROM `test1` ORDER BY `key2` ASC LIMIT 100");
+                rows.should.match([
+                    { id: 1, key2: 0.5, key3: "{\"foo\":\"bar\"}" },
+                    { id: 2, key2: 0.5, key3: "{\"foo\":\"bar\"}" },
+                    { id: 3, key2: 0.5, key3: "{\"foo\":\"bar\"}" },
+                    { id: 4, key2: 0.5, key3: "{\"foo\":\"bar\"}" }
+                ]);
+                done();
+            }, adapter.queryToOptions(query, {}));
+        });
+    
+        describe("single", function() {
+            it("no limit", function(done) {
+                const query = new Query(model).fields("key1,key2,key3").order("key2 asc");
+    
+                adapter.findWithNoCache(query.model, function(err, rows, extra) {
+                    should.ifError(err);
+                    extra.should.equal("SELECT `id`, `key2`, `key3` FROM `test1` ORDER BY `key2` " +
+                        "ASC LIMIT 0, 1");
+                    rows.should.match({ id: 1, key2: 0.5, key3: "{\"foo\":\"bar\"}" });
+                    done();
+                }, adapter.queryToOptions(query, { single: true }));
+            });
+    
+            it("limit 10", function(done) {
+                const query = new Query(model).fields("key1,key2,key3").order("key2 asc").limit(10);
+    
+                adapter.findWithNoCache(query.model, function(err, rows, extra) {
+                    should.ifError(err);
+                    extra.should.equal("SELECT `id`, `key2`, `key3` FROM `test1` ORDER BY `key2` " +
+                        "ASC LIMIT 1");
+                    rows.should.match({ id: 1, key2: 0.5, key3: "{\"foo\":\"bar\"}" });
+                    done();
+                }, adapter.queryToOptions(query, { single: true }));
+            });
+    
+            it("limit 1, 100", function(done) {
+                const query = new Query(model).fields("key1,key2,key3").order("key2 asc").limit(1, 100);
+    
+                adapter.findWithNoCache(query.model, function(err, rows, extra) {
+                    should.ifError(err);
+                    extra.should.equal("SELECT `id`, `key2`, `key3` FROM `test1` ORDER BY `key2` " +
+                        "ASC LIMIT 1, 1");
+                    rows.should.match({ id: 2, key2: 0.5, key3: "{\"foo\":\"bar\"}" });
+                    done();
+                }, adapter.queryToOptions(query, { single: true }));
+            });
+    
+            it("limit 100, 100", function(done) {
+                const query = new Query(model).fields("key1,key2,key3").order("key2 asc").limit(100, 100);
+    
+                adapter.findWithNoCache(query.model, function(err, rows, extra) {
+                    should.ifError(err);
+                    extra.should.equal("SELECT `id`, `key2`, `key3` FROM `test1` ORDER BY `key2` " +
+                        "ASC LIMIT 100, 1");
+                    should(rows).equal(null);
+                    done();
+                }, adapter.queryToOptions(query, { single: true }));
+            });
+        });
+    });
+    
+    describe(`${name} findWithCache`, function() {
+        const toshihiko = new Toshihiko("mysql", options);
+        const adapter = toshihiko.adapter;
+        const model = toshihiko.define("test1", common.COMMON_SCHEMA, {
+            cache: {
+                name: "memcached",
+                servers: [ "localhost:11211" ],
+                options: { prefix: "findWithCache_" }
+            }
+        });
+    
+        after(function() {
+            model.cache.memcached.flush(function() {
+                adapter.mysql.end();
+            });
+        });
+    
+        it("normal 1", function(done) {
+            const query = new Query(model).fields("key1,key2,key3,key5").order("key1 asc").limit(1);
+            adapter.findWithCache(model.cache, model, function(err, rows, extra) {
+                should.ifError(err);
+                extra.should.equal("SELECT `id`, `key2`, `key3`, `key5` FROM `test1` " +
+                    "ORDER BY `id` ASC LIMIT 1");
+                rows.should.match([
+                    { id: 1, key2: 0.5, key3: "{\"foo\":\"bar\"}" }
+                ]);
+                model.cache.memcached.get("findWithCache___toshihiko__:test1:1", function(err, res) {
+                    should.ifError(err);
+                    res.should.match({ id: 1,
+                        key2: 0.5,
+                        key3: "{\"foo\":\"bar\"}",
+                        key4: null,
+                        key5: rows[0].key5.toISOString(),
+                        key6: "10101000"
+                    });
+    
+                    model.cache.memcached.get("findWithCache___toshihiko__:test1:2", function(err, res) {
+                        should.ifError(err);
+                        should(res).equal(undefined);
+                        done();
+                    });
+                });
+            }, adapter.queryToOptions(query, {}));
+        });
+    
+        it("normal 2", function(done) {
+            const query = new Query(model).fields("key1,key2,key3,key5").order("key2 asc").limit(100);
+    
+            adapter.findWithCache(model.cache, model, function(err, rows, extra) {
+                should.ifError(err);
+                extra.should.equal("SELECT `id`, `key2`, `key3`, `key5` FROM `test1` " +
+                    "ORDER BY `key2` ASC LIMIT 100");
+                rows.should.match([
+                    { id: 1, key2: 0.5, key3: "{\"foo\":\"bar\"}" },
+                    { id: 2, key2: 0.5, key3: "{\"foo\":\"bar\"}" },
+                    { id: 3, key2: 0.5, key3: "{\"foo\":\"bar\"}" },
+                    { id: 4, key2: 0.5, key3: "{\"foo\":\"bar\"}" }
+                ]);
+    
+                model.cache.memcached.get("findWithCache___toshihiko__:test1:2", function(err, res) {
+                    should.ifError(err);
+                    res.should.match({
+                        key2: 0.5,
+                        key3: "{\"foo\":\"bar\"}",
+                        key4: "dummy primary",
+                        key5: rows[1].key5.toISOString(),
+                        key6: "100100010111010000000011011001",
+                        id: 2
+                    });
+                    done();
+                });
+            }, adapter.queryToOptions(query, {}));
+        });
+    });
+};
---
+++ test/adapters/mysql_insert.js
@@ -0,0 +1,240 @@
+/**
+ * XadillaX created at 2016-10-01 11:50:09 With ♥
+ *
+ * Copyright (c) 2016 Souche.com, all rights
+ * reserved.
+ */
+"use strict";
+
+const cu = require("config.util");
+const should = require("should");
+
+const hack = require("../util/hack");
+const common = require("../util/common");
+const Toshihiko = require("../../lib/toshihiko");
+
+module.exports = function(name, options) {
+    describe(`${name} insert`, function() {
+        describe("common schema", function() {
+            const toshihiko = new Toshihiko("mysql", options);
+            const adapter = toshihiko.adapter;
+            const model = toshihiko.define("test1", common.COMMON_SCHEMA);
+    
+            after(function() {
+                adapter.mysql.end();
+            });
+    
+            it("should insert 1", function(done) {
+                const now = new Date();
+                now.setMilliseconds(0);
+    
+                const hacked = hack.whereOnce(model, { key1: 1 });
+                adapter.insert(model, {
+                    key2: 0.5,
+                    key3: { foo: "bar" },
+                    key4: null,
+                    key5: now,
+                    key6: { dec: 168 },
+                    $123: 1,
+                    ok: function() {}
+                }, function(err, _row) {
+                    should.ifError(err);
+                    const row = cu.extendDeep({}, _row);
+                    row.should.match({
+                        key1: 1,
+                        key2: 0.5,
+                        key3: { foo: "bar" },
+                        key4: null,
+                        key5: now,
+                        key6: { dec: 168 }
+                    });
+                    hacked.called.should.equal(1);
+                    done();
+                });
+            });
+        });
+    
+        describe("ai is not pk", function() {
+            const toshihiko = new Toshihiko("mysql", options);
+            const adapter = toshihiko.adapter;
+            const model = toshihiko.define("test1", common.COMMON_SCHEMA_AI_IS_NOT_PRIMARY);
+    
+            after(function() {
+                adapter.mysql.end();
+            });
+    
+            it("should insert 1", function(done) {
+                const now = new Date();
+                now.setMilliseconds(0);
+    
+                const hacked = hack.whereOnce(model, { key4: "dummy primary" });
+                adapter.insert(model, {
+                    key2: 0.5,
+                    key3: { foo: "bar" },
+                    key4: "dummy primary",
+                    key5: now,
+                    key6: { dec: 610074841 }
+                }, function(err, _row) {
+                    should.ifError(err);
+                    const row = cu.extendDeep({}, _row);
+                    row.should.match({
+                        key1: 2,
+                        key2: 0.5,
+                        key3: { foo: "bar" },
+                        key4: "dummy primary",
+                        key5: now,
+                        key6: { dec: 610074841 }
+                    });
+    
+                    hacked.called.should.equal(1);
+                    done();
+                });
+            });
+        });
+    
+        describe("multi-primary-keys", function() {
+            const toshihiko = new Toshihiko("mysql", options);
+            const adapter = toshihiko.adapter;
+            const model = toshihiko.define("test1", common.COMMON_SCHEMA_MULTI_PRIMARY);
+    
+            after(function() {
+                adapter.mysql.end();
+            });
+    
+            it("should insert 1", function(done) {
+                const now = new Date();
+                now.setMilliseconds(0);
+    
+                const hacked = hack.whereOnce(model, { key1: 3, key4: "dummy multi primary" });
+                adapter.insert(model, {
+                    key2: 0.5,
+                    key3: { foo: "bar" },
+                    key4: "dummy multi primary",
+                    key5: now,
+                    key6: { dec: 8644325 }
+                }, function(err, _row) {
+                    should.ifError(err);
+                    const row = cu.extendDeep({}, _row);
+                    row.should.match({
+                        key1: 3,
+                        key2: 0.5,
+                        key3: { foo: "bar" },
+                        key4: "dummy multi primary",
+                        key5: now,
+                        key6: { dec: 8644325 }
+                    });
+    
+                    hacked.called.should.equal(1);
+                    done();
+                });
+            });
+        });
+    
+        describe("no primary key", function() {
+            const toshihiko = new Toshihiko("mysql", options);
+            const adapter = toshihiko.adapter;
+            const model = toshihiko.define("test1", common.COMMON_SCHEMA_NO_PRIMARY);
+    
+            after(function() {
+                adapter.mysql.end();
+            });
+    
+            it("should insert 1", function(done) {
+                const now = new Date();
+                now.setMilliseconds(0);
+    
+                const hacked = hack.whereOnce(model, {
+                    key1: 4,
+                    key2: 0.5,
+                    key3: { foo: "bar" },
+                    key4: "dummy no primary",
+                    key5: now,
+                    key6: { dec: 8644325 }
+                });
+                adapter.insert(model, {
+                    key2: 0.5,
+                    key3: { foo: "bar" },
+                    key4: "dummy no primary",
+                    key5: now,
+                    key6: { dec: 8644325 }
+                }, function(err, _row) {
+                    should.ifError(err);
+                    const row = cu.extendDeep({}, _row);
+                    row.should.match({
+                        key1: 4,
+                        key2: 0.5,
+                        key3: { foo: "bar" },
+                        key4: "dummy no primary",
+                        key5: now,
+                        key6: { dec: 8644325 }
+                    });
+    
+                    hacked.called.should.equal(1);
+                    done();
+                });
+            });
+        });
+    
+        describe("no ai key", function() {
+            const toshihiko = new Toshihiko("mysql", options);
+            const adapter = toshihiko.adapter;
+            const model = toshihiko.define("test2", common.NO_AI_SCHEMA);
+    
+            after(function() {
+                adapter.mysql.end();
+            });
+    
+            it("should insert 1", function(done) {
+                const now = new Date();
+                now.setMilliseconds(0);
+    
+                const hacked = hack.whereOnce(model, { key1: 1 });
+                adapter.insert(model, {
+                    key1: 1,
+                    key2: 0.5,
+                }, function(err, _row) {
+                    should.ifError(err);
+                    const row = cu.extendDeep({}, _row);
+                    row.should.match({
+                        key1: 1,
+                        key2: 0.5
+                    });
+    
+                    hacked.called.should.equal(1);
+                    done();
+                });
+            });
+        });
+    
+        describe("no ai key with no primary key", function() {
+            const toshihiko = new Toshihiko("mysql", options);
+            const adapter = toshihiko.adapter;
+            const model = toshihiko.define("test2", common.NO_AI_SCHEMA_WITH_NO_PRIMARY);
+    
+            after(function() {
+                adapter.mysql.end();
+            });
+    
+            it("should insert 1", function(done) {
+                const now = new Date();
+                now.setMilliseconds(0);
+    
+                const hacked = hack.whereOnce(model, { key1: 2, key2: 1 });
+                adapter.insert(model, {
+                    key1: 2,
+                    key2: 1,
+                }, function(err, _row) {
+                    should.ifError(err);
+                    const row = cu.extendDeep({}, _row);
+                    row.should.match({
+                        key1: 2,
+                        key2: 1
+                    });
+    
+                    hacked.called.should.equal(1);
+                    done();
+                });
+            });
+        });
+    });
+};
