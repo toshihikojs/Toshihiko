2fee9a1fafe4d812437495962944709556aa8036
XadillaX <admin@xcoder.in>
2016-10-10 19:37:32 +0800

    test: update for mysql adapter

--- test/adapters/mysql_update.js
+++ test/adapters/mysql_update.js
@@ -125,7 +125,9 @@ module.exports = function(name, options) {
         });
 
         after(function(done) {
-            adapter.mysql.end(done);
+            model.cache.memcached.flush(function() {
+                adapter.mysql.end(done);
+            });
         });
 
         it("should do delete cache", function(done) {
@@ -198,6 +200,174 @@ module.exports = function(name, options) {
                         }]);
                         done();
                     });
+                });
+            });
+        });
+    });
+
+    describe(`${name} update`, function() {
+        const toshihiko = new Toshihiko("mysql", options);
+        const adapter = toshihiko.adapter;
+        const model = toshihiko.define("test1", common.COMMON_SCHEMA, {
+            cache: {
+                name: "memcached",
+                servers: [ "localhost:11211" ],
+                options: { prefix: "update_" }
+            }
+        });
+
+        let forth;
+        before(function(done) {
+            const query = new Query(model);
+            query.find(function(err, result) {
+                should.ifError(err);
+                forth = result[3];
+                model.cache.getData("__toshihiko__", "test1", [ { id: 1 } ], function(err, result) {
+                    should.ifError(err);
+                    result.should.match([{
+                        id: 1,
+                        key2: 0.5
+                    }]);
+                    done();
+                });
+            });
+        });
+
+        after(function(done) {
+            model.cache.memcached.flush(function() {
+                adapter.mysql.end(done);
+            });
+        });
+
+        describe("with cache", function() {
+            it("should do delete cache", function(done) {
+                const $deleteKeys = model.cache.deleteKeys;
+                const $execute = adapter.execute;
+                let deleteKeysCalled = 0;
+                let executeCalled = 0;
+
+                model.cache.deleteKeys = function(database, name, keys, callback) {
+                    database.should.equal("__toshihiko__");
+                    name.should.equal("test1");
+                    keys.should.match([
+                        { id: 4 }
+                    ]);
+                    model.cache.deleteKeys = $deleteKeys;
+                    deleteKeysCalled++;
+                    callback();
+                };
+
+                adapter.execute = function(sql, callback) {
+                    sql.should.equal("SELECT `id` FROM `test1` WHERE (`id` = 4) LIMIT 0, 1");
+                    adapter.execute = $execute;
+                    executeCalled++;
+                    return $execute.call(adapter, sql, callback);
+                };
+
+                adapter.update(model, { key1: 4 }, [{
+                    field: model.fieldNamesMap.key2,
+                    value: 0.5
+                }], function(err, extra) {
+                    should.ifError(err);
+                    extra.should.equal("UPDATE `test1` SET `key2` = 0.5 WHERE `id` = 4");
+                    done();
+                });
+            });
+
+            it("should really delete cache", function(done) {
+                adapter.update(model, { key1: 4 }, [
+                    { field: model.fieldNamesMap.key1, value: 100 },
+                    { field: model.fieldNamesMap.key2, value: 100.5 },
+                    { field: model.fieldNamesMap.key3, value: { baz: "bbb" } },
+                    { field: model.fieldNamesMap.key4, value: "updated" },
+                    { field: model.fieldNamesMap.key6, value: { dec: 152 } }
+                ], function(err, extra) {
+                    should.ifError(err);
+                    extra.should.equal("UPDATE `test1` SET `id` = 100, `key2` = 100.5, `key3` = " +
+                        "'{\\\"baz\\\":\\\"bbb\\\"}', `key4` = 'updated', `key6` = BIN(152) WHERE `id` = 4");
+                    model.cache.getData("__toshihiko__", "test1", [ { id: 4 } ], function(err, result) {
+                        should.ifError(err);
+                        result.should.deepEqual([]);
+
+                        model.findById(100, function(err, yukari) {
+                            should.ifError(err);
+                            yukari.should.match({
+                                key1: 100,
+                                key2: 100.5,
+                                key3: { baz: "bbb" },
+                                key4: "updated",
+                                key6: { dec: 152 }
+                            });
+                            done();
+                        }, true);
+                    });
+                });
+            });
+        });
+
+        describe("without cache", function() {
+            const model = toshihiko.define("test1", common.COMMON_SCHEMA);
+
+            it("should update directly", function(done) {
+                adapter.update(model, { key1: 100 }, [
+                    { field: model.fieldNamesMap.key1, value: 4 },
+                    { field: model.fieldNamesMap.key2, value: 0.5 },
+                    { field: model.fieldNamesMap.key3, value: { foo: "bar" } },
+                    { field: model.fieldNamesMap.key4, value: "dummy no primary" },
+                    { field: model.fieldNamesMap.key6, value: { dec: 8644325 } }
+                ], function(err, extra) {
+                    should.ifError(err);
+                    extra.should.equal("UPDATE `test1` SET `id` = 4, `key2` = 0.5, `key3` = '{\\\"foo\\\":" +
+                        "\\\"bar\\\"}', `key4` = 'dummy no primary', `key6` = BIN(8644325) WHERE `id` = 100");
+                    model.findById(4, function(err, yukari) {
+                            should.ifError(err);
+                            yukari.should.match({
+                                key1: 4,
+                                key2: 0.5,
+                                key3: { foo: "bar" },
+                                key4: "dummy no primary",
+                                key6: { dec: 8644325 }
+                            });
+                            done();
+                    });
+                });
+            });
+
+            it("should be out-dated", function(done) {
+                adapter.update(model, { key1: 100 }, [{
+                    field: model.fieldNamesMap.key1,
+                    value: 4
+                }], function(err, extra) {
+                    err.message.should.equal("Out-dated yukari data.");
+                    extra.should.equal("UPDATE `test1` SET `id` = 4 WHERE `id` = 100");
+                    done();
+                });
+            });
+        });
+
+        describe("invalid parameters", function() {
+            it("should get error", function(done) {
+                adapter.update(model, {}, null, function(err) {
+                    err.message.should.equal("Invalid parameters.");
+
+                    adapter.update(model, null, {}, function(err) {
+                        err.message.should.equal("Invalid parameters.");
+                        done();
+                    });
+                });
+            });
+
+            it("yukari should broken", function(done) {
+                adapter.update(model, {}, [], function(err) {
+                    err.message.should.equal("Broken yukari object.");
+                        done();
+                });
+            });
+
+            it("update object should broken", function(done) {
+                adapter.update(model, { key1: 1 }, [], function(err) {
+                    err.message.should.equal("Broken update data information.");
+                        done();
                 });
             });
         });
